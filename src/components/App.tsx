6302de17e885c173177ccf2b9bba6cb87afc4837
Paola Becerra <paonata_marquez@outlook.com>
2021-09-26 16:03:40 -0500

    add main dashboard

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./organisms/Dashboard";
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/organisms/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import CommonPageLayout from "../CommonPageLayout";
+
+const Dashboard: React.FC = () => {
+  return (
+    <CommonPageLayout>
+      <div>Dashboard principal</div>
+    </CommonPageLayout>
+  );
+};
+
+export default Dashboard;
