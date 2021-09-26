5185722af6b543c2229a761a677f9203a0881ed7
Daniela Brito <danielaplanchart1@gmail.com>
2021-09-26 16:53:48 -0500

    Se creó página de Registro

---
+++ src/components/organisms/AuthRegister/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import CommonPageLayout from "../CommonPageLayout";
+
+const AuthRegister: React.FC = () => {
+  return (
+    <CommonPageLayout>
+      <div>Página de Registro</div>
+    </CommonPageLayout>
+  );
+};
+
+export default AuthRegister;
---
+++ src/pages/register.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import AuthRegister from "../components/organisms/AuthRegister";
+const RegisterPage: React.FC = () => {
+  return <AuthRegister/>
+};
+
+export default RegisterPage;
