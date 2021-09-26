3bb55ea51a14e34adbf70233038af6e775accc60
Daniela Brito <danielaplanchart1@gmail.com>
2021-09-26 17:55:22 -0500

    Crea la vista del formulario de registro

--- src/components/organisms/AuthRegister/index.tsx
+++ src/components/organisms/AuthRegister/index.tsx
@@ -1,10 +1,30 @@
 import * as React from "react";
 import CommonPageLayout from "../CommonPageLayout";
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const AuthRegister: React.FC = () => {
   return (
     <CommonPageLayout>
-      <div>Página de Registro</div>
+       <Form>
+        <Label>Registro</Label>
+      <FormGroup>
+        <Label for="email`">Correo electrónico</Label>
+        <Input type="email" name="email" id="email" placeholder="" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="name">Nombre</Label>
+        <Input type="text" name="name" id="Name" placeholder="" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="address">Direccion</Label>
+        <Input type="text" name="address" id="address" placeholder="" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="password">Contraseña</Label>
+        <Input type="text" name="password" id="password" placeholder="" />
+      </FormGroup>
+      <Button>Ingresar</Button>
+    </Form>
     </CommonPageLayout>
   );
 };
