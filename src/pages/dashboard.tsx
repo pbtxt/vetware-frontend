af2932cb4fe2470a5746b24b44b86ea3592247a9
Paola Becerra <paonata_marquez@outlook.com>
2021-09-25 20:10:50 -0500

    add dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Dashboard from "../components/organisms/Dashboard";
+
+const DashboardPage: React.FC = () => {
+  return <Dashboard />;
+};
+
+export default DashboardPage;
