376ffdb20f5a2f10d4ac376b520a8718b1a8cbf0
Paola Becerra <paonata_marquez@outlook.com>
2021-09-25 20:10:32 -0500

    add header basic structure

---
+++ src/components/molecules/Header/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <span>icono</span>
+        <div>
+          <span>Dashboard</span>
+          <span>Mis mascotas</span>
+        </div>
+      </div>
+      <div>
+        <span>Notificaciones</span>
+        <span>Ayuda</span>
+        <span>Citas</span>
+        <span>imagen</span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
