32c3e4c81a99766ab79b919c8d77d1ee45a9820b
Paola Becerra <paonata_marquez@outlook.com>
2021-09-26 17:24:01 -0500

    add common components styles

---
+++ src/assets/styles/CommonComponents/styles.ts
@@ -0,0 +1,8 @@
+import { Button } from "reactstrap";
+import styled from "styled-components";
+import { SECONDARY_COLOR } from "../colors";
+
+export const ActionButton = styled(Button)`
+  background-color: ${SECONDARY_COLOR};
+  border-color: ${SECONDARY_COLOR};
+`;
