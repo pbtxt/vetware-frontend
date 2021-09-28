7b7d7ce5f32c71d105a7613ed139118178631bc7
Paola Becerra <paonata_marquez@outlook.com>
2021-09-27 21:34:24 -0500

    add main page

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import App from "../components/App";
+import "../components/atoms/NewPetCard/styles.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
