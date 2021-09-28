45e4d4af31586cd11f2d0214a993bebb6fddfb65
Paola Becerra <paonata_marquez@outlook.com>
2021-09-28 09:51:28 -0500

    add new pet function

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -9,3 +9,21 @@ export async function getUsers() {
     return data.json();
   });
 }
+
+export async function createNewPet(owner_email, name, species, breed) {
+  return fetch("http://localhost:4000/api/pets", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      owner_email,
+      name,
+      species,
+      breed,
+    }),
+  }).then((data) => {
+    return data.json();
+  });
+}
